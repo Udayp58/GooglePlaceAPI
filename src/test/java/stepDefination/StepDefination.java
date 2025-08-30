ee6883adc69f710a46c199e32c52b166e346994e
UdayPatil <udaypatil58@gmail.com>
2025-08-30 20:00:20 +0530

    Art in stepdefination

--- src/test/java/stepDefination/StepDefination.java
+++ src/test/java/stepDefination/StepDefination.java
@@ -32,7 +32,8 @@ public class StepDefination {
 		APIResource resourceAPI = APIResource.valueOf(resource);
 		System.out.println(resourceAPI.getResource());
 		System.out.println("This new Line Added");
-		System.out.println("This new line added by uday");
+	//	System.out.println("This new line added by uday");
+	// commented in Develop branch
 
 		if (method.equalsIgnoreCase("POST")) {
 			Res = Request.when().post(resourceAPI.getResource());
